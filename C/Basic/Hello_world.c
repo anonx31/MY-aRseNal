b62a8877cfd3c8ddd85d92b65e926da782295c9a
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-03-30 12:05:05 +0530

    Update Hello_world.c

--- C/Basic/Hello_world.c
+++ C/Basic/Hello_world.c
@@ -1,5 +1,12 @@
+//Simple C program to display "Hello World"
+
+//Header file
 #include <stdio.h>
+
+//start of main function -
+//execution of program start from main function
 int main () {
+	//this will print "Hello World!"
 	printf("Hello World!");
 	return 0;
 }
