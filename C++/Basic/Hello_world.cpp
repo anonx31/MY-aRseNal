646d4e256ae4a5ddcc105532a1e6ee540cc0d180
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-03-30 12:08:24 +0530

    Update Hello_world.cpp

--- C++/Basic/Hello_world.cpp
+++ C++/Basic/Hello_world.cpp
@@ -1,5 +1,12 @@
+//Simple C program to display "Hello World"
+
+//Header file
 #include<iostream>
+
+//start of main function
+//exection will start from here
 int main () {
-	std::cout<<"Hello World!!!"<<std::endl;
+	//print "Hello World"
+	std::cout<<"Hello World!"<<std::endl;
 	return 0;
 }
