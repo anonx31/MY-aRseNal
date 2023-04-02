3dd915cecd9125f245dc672ed6952b111984e43b
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-02 11:17:59 -0700

    Add files via upload

---
+++ C++/Basic/Print_integer.cpp
@@ -0,0 +1,13 @@
+/*program to print integer in c using cin*/
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	//will print message
+	cout<<"Enter any integer:";
+	//will scan integer
+	cin>>n;
+	//will print integer
+	cout<<"You entered "<<n<<endl;
+	return 0;
+}
