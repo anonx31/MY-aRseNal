f55953d931826aa4ffc4961518475daf4707af3f
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-02 11:00:30 -0700

    Add files via upload

---
+++ C/Basic/Print_integer.c
@@ -0,0 +1,11 @@
+/*program to print your own name in C by scanf function*/
+#include<stdio.h>
+int main() {
+	int n;
+	//will print message
+	printf("Enter any integer:");
+	//will scan integer
+	scanf("%d",&n);
+	//will print integer
+	printf("You entered %d",n);
+}
---
+++ C/Basic/Print_name.c
@@ -0,0 +1,11 @@
+/*program to print your own name in C by scanf function*/
+#include<stdio.h>
+int main() {
+	char name[20];
+	//will print message
+	printf("Enter your name:");
+	//will scan name
+	scanf("%s",&name);
+	//will print name
+	printf("Your name is %s",name);
+}
