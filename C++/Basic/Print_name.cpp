b7054a0bd6a3b0a574b81c78724184a929e36042
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-02 11:12:35 -0700

    Add files via upload

---
+++ C++/Basic/Print_name.cpp
@@ -0,0 +1,13 @@
+/*program to print your own name in c using cin*/
+#include<iostream>
+using namespace std;
+int main() {
+	char name[20];
+	//will print message
+	cout<<"Enter your name:";
+	//will scan name
+	cin>>name;
+	//will print name
+	cout<<"Your name is "<<name<<endl;
+	return 0;
+}
