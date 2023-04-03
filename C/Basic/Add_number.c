4b34bf77776b4e6ee3cc4f87756a14a8596aab3c
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-03 06:07:07 -0700

    Add files via upload

---
+++ C/Basic/Add_number.c
@@ -0,0 +1,12 @@
+/*program to add to integers in c*/
+#include<stdio.h>
+int main() {
+	int a,b;
+	printf("Enter first integer: ");
+	scanf("%d",&a);
+	printf("Enter second integer: ");
+	scanf("%d",&b);
+	int sum=a+b;
+	printf("Addition: %d",sum);
+	return 0;
+}
