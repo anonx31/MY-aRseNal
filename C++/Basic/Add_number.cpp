282e9370dd86875c9d5d33475c80b05ee45d62f0
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-03 06:07:32 -0700

    Add files via upload

---
+++ C++/Basic/Add_number.cpp
@@ -0,0 +1,13 @@
+/*program to add two integer in c++*/
+#include<iostream>
+using namespace std;
+int main() {
+	int a,b;
+	cout<<"Enter first integer: ";
+	cin>>a;
+	cout<<"Enter second integer: ";
+	cin>>b;
+	int sum=a+b;
+	cout<<"Addition: "<<sum<<endl;
+	return 0;
+}
