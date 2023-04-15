1c92ca8a8d9e8965f35acdde8af7adfe6ca666ff
Anon_x_31 <99769246+anonx31@users.noreply.github.com>
2023-04-15 20:30:21 +0530

    Add files via upload

---
+++ C++/Basic/ASCII_value.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+ 	char c;
+ 	cout<<"Enter any character: ";
+ 	cin>>c;
+ 	cout<<"ASCII value of character "<<c<<" is "<<int(c);
+ 	return 0;
+}
